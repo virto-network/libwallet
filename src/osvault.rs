af99270a5b60794dc2b1bc5853e1580642d904b5
Gabi <gabiazcona@protonmail.com>
2022-04-09 20:02:11 +0200

    OSVault create: return seed created and add a doctest

--- src/osvault.rs
+++ src/osvault.rs
@@ -9,15 +9,24 @@ pub struct OSVault<P: Pair> {
 }
 
 impl<P: Pair> OSVault<P> {
-    // Create new entry with a random seed and save it in the OS.
-    pub fn create(name: &str) -> Result<Self> {
+    /// Create new entry with a random seed and save it in the OS.
+    /// ```
+    /// # use libwallet::{OSVault, Vault, Result, sr25519};
+    /// # #[async_std::main] async fn main() -> Result<()> {
+    /// let (mut vault, seed) = OSVault::<sr25519::Pair>::create("libwallet-doctest").unwrap();
+    /// println!("seed: {}", seed);
+    /// assert!(vault.unlock(()).await.is_ok());
+    /// assert!(!seed.is_empty());
+    /// # Ok(()) }
+    /// ```
+    pub fn create(name: &str) -> Result<(Self, String)> {
         let entry = keyring::Entry::new("wallet", &name);
         let (_, phrase, seed) = P::generate_with_phrase(None);
         entry.set_password(&phrase).map_err(|_| Error::InvalidPhrase)?;
-        Ok(OSVault { 
+        Ok((OSVault { 
             entry, 
             seed: Some(seed) 
-        })
+        }, phrase))
     }
     
     // Create new password saved in OS with given name.
